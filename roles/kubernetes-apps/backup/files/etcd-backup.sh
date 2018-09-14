c8d05d222a2e8b5a675802344992446691aa6f2c
sorididim11 <sorididim11@gmail.com>
2018-09-14 09:26:26 +0900

    added backup script for etcd

--- roles/kubernetes-apps/backup/files/etcd-backup.sh
+++ roles/kubernetes-apps/backup/files/etcd-backup.sh
@@ -8,4 +8,4 @@ ETCDCTL_API=3 etcdctl --endpoints $ETCD_LISTEN_CLIENT_URLS --cacert   $ETCD_TRUS
 
 # backup 
 ETCDCTL_API=2 etcdctl --endpoints $ETCD_LISTEN_CLIENT_URLS --ca-file $ETCD_TRUSTED_CA_FILE --cert-file $ETCD_CERT_FILE --key-file $ETCD_KEY_FILE backup --data-dir $ETCD_DATA_DIR --backup-dir ./backup	
-ETCDCTL_API=3 etcdctl --endpoints $ETCD_LISTEN_CLIENT_URLS --cacert $ETCD_TRUSTED_CA_FILE --cert $ETCD_CERT_FILE --key $ETCD_KEY_FILE snapshot save .backup/member/snap/db
+ETCDCTL_API=3 etcdctl --endpoints $ETCD_LISTEN_CLIENT_URLS --cacert $ETCD_TRUSTED_CA_FILE --cert $ETCD_CERT_FILE --key $ETCD_KEY_FILE snapshot save ./backup/member/snap/db
