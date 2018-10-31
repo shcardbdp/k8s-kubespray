43f0e2c7be93f0933cc579037b1e13de42378dc2
sorididim11 <sorididim11@gmail.com>
2018-10-31 17:48:00 +0900

    first draft

--- nvidia/connector/entrypoint.sh
+++ nvidia/connector/entrypoint.sh
@@ -3,11 +3,12 @@ set -e
  
 # If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 9999 as default
 CURRENT_UID=${uid:-9999}
+CURRENT_GID=${gid:-9999}
  
 # Notify user about the UID selected
-echo "Current UID : $CURRENT_UID"
+echo "Current UID : $CURRENT_UID, Current GID: $CURRENT_GID"
 # Create user called "docker" with selected UID
-useradd --shell /bin/bash -u $CURRENT_UID -o -c ""  -m jupyter
+groupadd -g $CURRENT_GID -r jupyter && useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -g jupyter -m jupyter
 chown jupyter:jupyter /notebooks
  
 # Execute process
