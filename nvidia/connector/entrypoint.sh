cb5725f15e0a5718c5562ff44c358b6d67d4d154
sorididim11 <sorididim11@gmail.com>
2018-10-31 18:41:40 +0900

    first draft

--- nvidia/connector/entrypoint.sh
+++ nvidia/connector/entrypoint.sh
@@ -9,8 +9,8 @@ CURRENT_GID=${gid:-9999}
 # Notify user about the UID selected
 echo "Current UID : $CURRENT_UID, Current GID: $CURRENT_GID"
 # Create user called "docker" with selected UID
-#groupadd -g $CURRENT_GID -r jupyter && useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -g jupyter -m jupyter
-#chown jupyter:jupyter /notebooks
+groupadd -g $CURRENT_GID -r jupyter && useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -g jupyter -m jupyter
+chown jupyter:jupyter /notebooks
  
 # Execute process
-exec /usr/local/bin/gosu $(id -u) "$@"
+exec /usr/local/bin/gosu jupyter "$@"
