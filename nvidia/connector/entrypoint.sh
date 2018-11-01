83ada52e292da1e391bef88975bb8e6528e6d79e
sorididim11 <sorididim11@gmail.com>
2018-11-01 15:15:15 +0900

    first draft

--- nvidia/connector/entrypoint.sh
+++ nvidia/connector/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
 
 set -e
 
@@ -65,7 +63,7 @@ if [ $(id -u) == 0 ] ; then
     # Exec the command as NB_USER with the PATH and the rest of
     # the environment preserved
     echo "Executing the command: $cmd"
-    exec sudo -E -H -u $NB_USER PATH=$PATH PYTHONPATH=$PYTHONPATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH $cmd
+    exec sudo -E -H -u $NB_USER PATH=$PATH PYTHONPATH=$PYTHONPATH $cmd
 else
     if [[ "$NB_UID" == "$(id -u jupyter)" && "$NB_GID" == "$(id -g jupyter)" ]]; then
         # User is not attempting to override user/group via environment
