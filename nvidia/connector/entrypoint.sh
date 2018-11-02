9c921b677fd9b7312f32f60ad37c563a1ae46a25
sorididim11 <sorididim11@gmail.com>
2018-11-02 17:11:57 +0900

    first

--- nvidia/connector/entrypoint.sh
+++ nvidia/connector/entrypoint.sh
@@ -7,30 +7,6 @@ else
     cmd=$*
 fi
 
-run-hooks () {
-    # Source scripts or run executable files in a directory
-    if [[ ! -d "$1" ]] ; then
-        return
-    fi
-    echo "$0: running hooks in $1"
-    for f in "$1"/*; do
-        case "$f" in
-            *.sh)
-                echo "$0: running $f"
-                source "$f"
-                ;;
-            *)
-                if [[ -x "$f" ]] ; then
-                    echo "$0: running $f"
-                    "$f"
-                else
-                    echo "$0: ignoring $f"
-                fi
-                ;;
-        esac
-    echo "$0: done running hooks in $1"
-    done
-}
 
 
 # Handle special flags if we're root
