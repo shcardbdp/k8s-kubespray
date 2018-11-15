36d2ae38eb327cbe7c55a35f0a6233f8b1dfea2a
sorididim11 <sorididim11@gmail.com>
2018-11-15 19:01:14 +0900

    first

--- nvidia/image/build.sh
+++ nvidia/image/build.sh
@@ -31,7 +31,8 @@ sudo docker build -t sorididim11/ac52-lab-cpu:${VERSION} -f Dockerfile.ac52-lab 
 sudo docker build -t sorididim11/hobby-lab-cpu:${VERSION} -f Dockerfile.hobby-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} . && \
 sudo docker build -t sorididim11/dev-lab-cpu:${VERSION} -f Dockerfile.dev-all --build-arg ROOT_IMAGE=ufoym/deepo:all-py36-jupyter-cpu --build-arg VERSION=${VERSION} . && \
 sudo docker build -t sorididim11/spark-lab-cpu:${VERSION} -f Dockerfile.spark-lab --build-arg VERSION=${VERSION} . && \
-sudo docker build -t sorididim11/r-lab-cpu:${VERSION} -f Dockerfile.r-lab  --build-arg VERSION=${VERSION} .
+sudo docker build -t sorididim11/r-lab-cpu:${VERSION} -f Dockerfile.r-lab  --build-arg VERSION=${VERSION} . && \
+sudo docker build -t sorididim11/mini-lab-cpu:${VERSION} -f Dockerfile.mini-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} .
 
 
 
@@ -39,4 +40,5 @@ sudo docker build -t sorididim11/r-lab-cpu:${VERSION} -f Dockerfile.r-lab  --bui
 sudo docker build -t sorididim11/dl-base-gpu:${VERSION} -f Dockerfile.dl-base  --build-arg VERSION=${VERSION} . && \
 sudo docker build -t sorididim11/ac52-lab-gpu:${VERSION} -f Dockerfile.ac52-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .  && \
 sudo docker build -t sorididim11/hobby-lab-gpu:${VERSION} -f Dockerfile.hobby-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .  && \
-sudo docker build -t sorididim11/dev-lab-gpu:${VERSION} -f Dockerfile.dev-all --build-arg VERSION=${VERSION} .
+sudo docker build -t sorididim11/dev-lab-gpu:${VERSION} -f Dockerfile.dev-all --build-arg VERSION=${VERSION} . && \
+sudo docker build -t sorididim11/mini-lab-gpu:${VERSION} -f Dockerfile.mini-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .
