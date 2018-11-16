1e31cc76f59847bd0371eed6dc0fbddcdeb25dbe
sorididim11 <sorididim11@gmail.com>
2018-11-16 19:46:21 +0900

    first

--- nvidia/image/build.sh
+++ nvidia/image/build.sh
@@ -27,18 +27,18 @@ echo "VERSION = ${VERSION}"
 
 
 sudo docker build -t sorididim11/dl-base-cpu:${VERSION} -f Dockerfile.dl-base --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} .  && \
-sudo docker build -t sorididim11/mlbasic-lab-cpu:${VERSION} -f Dockerfile.ac52-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} . && \
-sudo docker build -t sorididim11/bdtf-lab-cpu:${VERSION} -f Dockerfile.hobby-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} . && \
-sudo docker build -t sorididim11/dl-lab-cpu:${VERSION} -f Dockerfile.dev-all --build-arg ROOT_IMAGE=ufoym/deepo:all-py36-jupyter-cpu --build-arg VERSION=${VERSION} . && \
+sudo docker build -t sorididim11/mlbasic-lab-cpu:${VERSION} -f Dockerfile.mlbasic-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} . && \
+sudo docker build -t sorididim11/bdtf-lab-cpu:${VERSION} -f Dockerfile.bdtf-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} . && \
+sudo docker build -t sorididim11/dl-lab-cpu:${VERSION} -f Dockerfile.dl-lab --build-arg ROOT_IMAGE=ufoym/deepo:all-py36-jupyter-cpu --build-arg VERSION=${VERSION} . && \
 sudo docker build -t sorididim11/spark-lab-cpu:${VERSION} -f Dockerfile.spark-lab --build-arg VERSION=${VERSION} . && \
 sudo docker build -t sorididim11/r-lab-cpu:${VERSION} -f Dockerfile.r-lab  --build-arg VERSION=${VERSION} . && \
-#sudo docker build -t sorididim11/mllight-lab-cpu:${VERSION} -f Dockerfile.mini-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} .
+#sudo docker build -t sorididim11/mllight-lab-cpu:${VERSION} -f Dockerfile.mllight-lab --build-arg DEVICE_TYPE=cpu --build-arg VERSION=${VERSION} .
 
 
 
 # GPU 
 sudo docker build -t sorididim11/dl-base-gpu:${VERSION} -f Dockerfile.dl-base --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} . && \
-sudo docker build -t sorididim11/mlbasic-lab-gpu:${VERSION} -f Dockerfile.ac52-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .  && \
-sudo docker build -t sorididim11/bdtf-lab-gpu:${VERSION} -f Dockerfile.hobby-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .  && \
-sudo docker build -t sorididim11/dl-lab-gpu:${VERSION} -f Dockerfile.dev-all --build-arg VERSION=${VERSION} . && \
-#sudo docker build -t sorididim11/mllight-lab-gpu:${VERSION} -f Dockerfile.mini-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .
+sudo docker build -t sorididim11/mlbasic-lab-gpu:${VERSION} -f Dockerfile.mlbasic-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .  && \
+sudo docker build -t sorididim11/bdtf-lab-gpu:${VERSION} -f Dockerfile.bdtf-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .  && \
+sudo docker build -t sorididim11/dl-lab-gpu:${VERSION} -f Dockerfile.dl-lab --build-arg VERSION=${VERSION} . && \
+#sudo docker build -t sorididim11/mllight-lab-gpu:${VERSION} -f Dockerfile.mllight-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${VERSION} .
