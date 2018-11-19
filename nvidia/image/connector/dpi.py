c6e12e8817a90ca857ef23cb91c3a5848c236a5d
sorididim11 <sorididim11@gmail.com>
2018-11-19 13:51:54 +0900

    hello

---
+++ nvidia/image/connector/dpi.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import time
+import random 
+import pandas as pd
+
+
+def dbGetQuery(query: str, source: str='oracle', sep: str=',', dtype=None):
+    """
+    데이터베이스에서 query 를 실행한 결과물을 DataFrame으로 반환한다. 
+
+    params
+    query: str, valid query statement
+    source: str, data source, hive or oracle
+    sep: str, 분할자, default ','
+
+    return 
+    df: DataFrame
+    예시:
+    df = dfGetQuery("SELECT * from SC202079.SHC_MCT")
+    """
+
+    PREFIX_OUTFILE = '/home/jovyan/notebooks/data/'
+    outfile = "temp%d.csv" % random.randint(0, 1e10)    
+    print('cmd: jdbc-cli -t %s -o %s -q "%s" -s %s' %(source, outfile, query, sep) )
+    os.system(command='jdbc-cli -t %s -o %s -q "%s" -s %s' %(source, outfile, query, sep))
+    df = pd.read_csv(PREFIX_OUTFILE + outfile, error_bad_lines=False, encoding='utf-8', sep=sep, dtype=dtype)
+    os.system(command='rm %s' % PREFIX_OUTFILE + outfile)
+
+    return df
+
+if __name__ == "__main__":
+    dbGetQuery(sys.argv[1], sys.argv[2], sys.argv[3])
