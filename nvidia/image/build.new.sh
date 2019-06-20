3e87a01a81d1c89b0e86c9d82880126da9376099
realspoon <realspoon@gmail.com>
2019-06-20 10:40:34 +0900

    Modify version of spark-notebook to 1.0.9

--- nvidia/image/build.new.sh
+++ nvidia/image/build.new.sh
@@ -3,24 +3,27 @@
 set -e
 
 # usage() { echo "Usage: $0 [-v image version] [-t <cpu/gpu/all> (default: all)]" 1>&2; exit 1; }
-usage() { echo "Usage: $0 [-b base image version (defaul DL_BASE_VERSION)] [-s build scope (all, ml, sp, r, mt, 2, 3)]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-h help] [-b base image version (defaul DL_BASE_VERSION)] [-s build scope (all, p, ml, sp, r, mt, 3)]" 1>&2; exit 1; }
 
 # image version
-DL_BASE_VERSION="1.0.2"
-MLLIGHT_VERSION="1.0.8"
-SPARK_VERSION="1.0.1"
-R_VERSION="1.0.1"
-TEXT_VERSION="1.0.2"
+DL_BASE_VERSION="1.0.3"
+MLLIGHT_VERSION="1.0.9"
+SPARK_VERSION="1.0.9"
+R_VERSION="1.0.2"
+TEXT_VERSION="1.0.3"
 VERSION=${DL_BASE_VERSION}
 
 # TYPE="all"
-while getopts ":b:s:" flag; do
+while getopts ":b:s:h:" flag; do
     case "${flag}" in
         b)
             VERSION=${OPTARG}
             ;;            
         s)
             SCOPE=${OPTARG}
+            ;;
+        h)
+            usage
             ;;
         *)
             echo "invalid args..."
@@ -49,14 +52,15 @@ sudo docker build -t shcardbdp/r-notebook:${R_VERSION} -f Dockerfile.r-notebook 
 sudo docker build -t shcardbdp/mllight-text-lab:${TEXT_VERSION} -f Dockerfile.mllight-text-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${TEXT_VERSION} --build-arg BASE_VERSION=${MLLIGHT_VERSION} .
 fi
 
-if [ "${SCOPE}" == "2" ]; then
-sudo docker build -t shcardbdp/mllight-lab-gpu:${MLLIGHT_VERSION} -f Dockerfile.mllight-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${MLLIGHT_VERSION} --build-arg BASE_VERSION=${VERSION} .
-fi
-
 if [ "${SCOPE}" == "3" ]; then
 sudo docker build -t shcardbdp/spark-notebook:${SPARK_VERSION} -f Dockerfile.spark-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${SPARK_VERSION} --build-arg BASE_VERSION=${MLLIGHT_VERSION} .
 sudo docker build -t shcardbdp/r-notebook:${R_VERSION} -f Dockerfile.r-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${R_VERSION} --build-arg BASE_VERSION=${MLLIGHT_VERSION} .
 sudo docker build -t shcardbdp/mllight-text-lab:${TEXT_VERSION} -f Dockerfile.mllight-text-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${TEXT_VERSION} --build-arg BASE_VERSION=${MLLIGHT_VERSION} .
+fi
+
+if [ "${SCOPE}" == "p" ]; then
+sudo docker build -t shcardbdp/dl-base-gpu:${DL_BASE_VERSION} -f Dockerfile.dl-base --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${DL_BASE_VERSION} . && \
+sudo docker build -t shcardbdp/mllight-lab-gpu:${MLLIGHT_VERSION} -f Dockerfile.mllight-lab --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${MLLIGHT_VERSION} --build-arg BASE_VERSION=${VERSION} .
 fi
 
 if [ "${SCOPE}" == "ml" ]; then
@@ -64,7 +68,7 @@ sudo docker build -t shcardbdp/mllight-lab-gpu:${MLLIGHT_VERSION} -f Dockerfile.
 fi
 
 if [ "${SCOPE}" == "sp" ]; then
-sudo docker build -t shcardbdp/spark-notebook:${SPARK_VERSION} -f Dockerfile.spark-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${SPARK_VERSION} --build-arg BASE_VERSION=${VERSION} .
+sudo docker build -t shcardbdp/spark-notebook:${SPARK_VERSION} -f Dockerfile.spark-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${SPARK_VERSION} --build-arg BASE_VERSION=${MLLIGHT_VERSION} .
 fi
 
 if [ "${SCOPE}" == "r" ]; then
