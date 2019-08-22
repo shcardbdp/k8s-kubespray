758a73387fd58e32e2ca36a88be20356eafe3f5a
realspoon <realspoon@gmail.com>
2019-08-22 14:29:29 +0900

    r-notebook 2.0.1

--- nvidia/image/build.new.2.sh
+++ nvidia/image/build.new.2.sh
@@ -9,7 +9,7 @@ usage() { echo "Usage: $0 [-h help] [-s build scope (a[ll], b[ase], sp[ark], t[e
 DATALAB_BASE_VERSION="1.0.1"
 SPARK_VERSION="2.0.1"
 TEXT_VERSION="1.0.1"
-# R_VERSION="1.0.3"
+R_VERSION="2.0.1"
 
 # TYPE="all"
 while getopts ":s:h:" flag; do
@@ -40,13 +40,14 @@ fi
 echo "DATALAB_BASE_VERSION = ${DATALAB_BASE_VERSION}"
 echo "SPARK_VERSION = ${SPARK_VERSION}"
 echo "TEXT_VERSION = ${TEXT_VERSION}"
-# echo "R_VERSION = ${R_VERSION}"
+echo "R_VERSION = ${R_VERSION}"
 
 
 if [ "${SCOPE}" == "a" ]; then
 sudo docker build -t shcardbdp/datalab-base-gpu:${DATALAB_BASE_VERSION} -f Dockerfile.dl-base2 --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${DATALAB_BASE_VERSION} . && \
 sudo docker build -t shcardbdp/spark-notebook:${SPARK_VERSION} -f Dockerfile.spark-notebook2 --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${SPARK_VERSION} --build-arg BASE_VERSION=${DATALAB_BASE_VERSION} . && \
-sudo docker build -t shcardbdp/text-notebook:${TEXT_VERSION} -f Dockerfile.text-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${TEXT_VERSION} --build-arg BASE_VERSION=${SPARK_VERSION} .
+sudo docker build -t shcardbdp/text-notebook:${TEXT_VERSION} -f Dockerfile.text-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${TEXT_VERSION} --build-arg BASE_VERSION=${SPARK_VERSION} . && \
+sudo docker build -t shcardbdp/r-notebook:${R_VERSION} -f Dockerfile.r-notebook2 --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${R_VERSION} --build-arg BASE_VERSION=${SPARK_VERSION} .
 fi
 
 if [ "${SCOPE}" == "b" ]; then
@@ -61,8 +62,8 @@ if [ "${SCOPE}" == "t" ]; then
 sudo docker build -t shcardbdp/text-notebook:${TEXT_VERSION} -f Dockerfile.text-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${TEXT_VERSION} --build-arg BASE_VERSION=${SPARK_VERSION} .
 fi
 
-# if [ "${SCOPE}" == "r" ]; then
-# sudo docker build -t shcardbdp/r-notebook:${R_VERSION} -f Dockerfile.r-notebook --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${R_VERSION} --build-arg BASE_VERSION=${SPARK_VERSION} .
-# fi
+if [ "${SCOPE}" == "r" ]; then
+sudo docker build -t shcardbdp/r-notebook:${R_VERSION} -f Dockerfile.r-notebook2 --build-arg DEVICE_TYPE=gpu --build-arg VERSION=${R_VERSION} --build-arg BASE_VERSION=${SPARK_VERSION} .
+fi
 
 
